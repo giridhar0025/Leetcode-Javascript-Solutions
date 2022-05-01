ef1bd009d43c684ee9525b04195ab2a0aac95e93
Giridhar <81301990+giridhar0025@users.noreply.github.com>
2022-05-01 16:23:37 +0530

    Update Remove_element.js

--- LeetCode-Solutions-JS/Remove_element.js
+++ LeetCode-Solutions-JS/Remove_element.js
@@ -58,4 +58,24 @@ var removeElement = function(nums, val) {
             i--
         }
     }
+ return nums.length
 };
+
+// Two Pointer Approach
+
+var removeElement = function(nums, val) {
+    let i = 0;
+    let j = 0
+    while (j < nums.length) {
+      if (nums[j] === val) {
+        j++
+      } else {
+        nums[i] = nums[j]
+        i++
+        j++
+      }
+    }
+    return i
+}
+
+
